6ee390f33aa71edcecb98427224647312e6404b4
CodeSpacePlus <bryanc0791@gmail.com>
2019-11-23 17:14:42 -0700

    Finished Vue + Typescript sample project

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Using Vuejs with TypeScript</h1>
+    <button @click="toggleHighlightedPostsVisibility">
+      {{ showHighlighted ? "Hide" : "Show" }} Highlighted Posts
+    </button>
+    <BlogPost
+      v-for="blogPost in visibleBlogPosts"
+      :post="blogPost"
+      :key="blogPost.title"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import BlogPost, { Post } from "./components/BlogPost.vue";
+import { AxiosResponse } from "axios";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    BlogPost
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public showHighlighted: boolean = false;
+
+  private blogPosts: Post[] = [
+    {
+      title: "My first blogpost ever!",
+      body: "Lorem ipsum dolor sit amet.",
+      author: "Elke",
+      datePosted: new Date(2019, 10, 2),
+      highlighted: false
+    },
+    {
+      title: "Look I am blogging!",
+      body: "This is my second post!",
+      author: "Elke",
+      datePosted: new Date(2019, 10, 15),
+      highlighted: true
+    },
+    {
+      title: "Another one",
+      body: "Anotha one!",
+      author: "Elke",
+      datePosted: new Date(2019, 11, 15),
+      highlighted: false
+    }
+  ];
+
+  get visibleBlogPosts() {
+    return this.blogPosts.filter(
+      (post: Post) =>
+        post.highlighted === undefined ||
+        post.highlighted === this.showHighlighted
+    );
+  }
+
+  public toggleHighlightedPostsVisibility() {
+    this.showHighlighted = !this.showHighlighted;
+  }
+
+  private created() {
+    this.axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response: AxiosResponse) => {
+        this.blogPosts = response.data.map((val: any) => ({
+          title: val.title,
+          body: val.body,
+          author: val.author,
+          datePosted: new Date(val.datePosted),
+          highlighted: val.highlighted
+        }));
+      });
+  }
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
